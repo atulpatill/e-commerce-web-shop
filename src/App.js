99b1dec139b2e76f3b5c25b87c50f14478c49e7c
atul patil <atulpatil539@gmail.com>
2022-02-07 20:17:02 +0530

    feat: Component added & dependencies installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
 import './App.css';
+import Homepage from './pages/Homepage';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     <h1>Web shop</h1>
+     <h1>Web-shop</h1>
+     <Homepage />
+     <button className='btn btn-primary'>BootStrap Btn</button>
     </div>
   );
 }
---
+++ src/pages/ProductInfo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ProductInfo() {
+  return <div>
+       <h1>ProductInfo</h1>
+  </div>;
+}
+
+export default ProductInfo;
