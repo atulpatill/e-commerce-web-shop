b161e741f5fdd129f317bc3926db0aa14a2b05fa
atul patil <atulpatil539@gmail.com>
2022-02-10 19:39:27 +0530

    feat : search filter added

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -7,82 +7,108 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
 function Homepage() {
+  const [loading, setLoading] = useState(false);
+  const { cartItems } = useSelector((state) => state.cartReducer);
+  const [searchKey, setSearchKey] = useState("");
+  const [filterType, setFilterType] = useState();
+  const navigate = useNavigate();
+  const [products, setproducts] = useState([]);
+  const dispatch = useDispatch();
 
-  const [loading, setLoading] = useState(false) 
-  const {cartItems} = useSelector(state => state.cartReducer)
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
 
- const navigate = useNavigate()
-  const [products , setproducts] = useState([])
-  const dispatch = useDispatch()
+  const addtocart = (product) => {
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: product,
+    });
+  };
 
-  useEffect(()=>{
-      localStorage.setItem('cartItems', JSON.stringify(cartItems))
-  },[cartItems])
-
-  const addtocart =(product) =>{
-           dispatch({
-             type: "ADD_TO_CART",
-             payload : product
-           })
-  }
-
-  useEffect(()=>{
-    getData()
-  },[])
+  useEffect(() => {
+    getData();
+  }, []);
 
   async function getData() {
-    
     try {
-      setLoading(true)
+      setLoading(true);
       const users = await getDocs(collection(fireDB, "products"));
       const productsArray = [];
       users.forEach((doc) => {
-     
         const obj = {
           id: doc.id,
           ...doc.data(),
         };
         productsArray.push(obj);
-        setLoading(false)
+        setLoading(false);
       });
-     setproducts(productsArray)
+      setproducts(productsArray);
     } catch (error) {
       console.log(error);
-      setLoading(false)
+      setLoading(false);
     }
   }
-   
-  
+
   return (
     <Layout loading={loading}>
-     
-    <div className="container">
-      <div className="row">
-        {products.map(product=>{
-          return <div className="col-md-4" key={product.id}>
-
-            <div className="m-2 p-1 product position-relative">
-            <div className="product-content">
-            <p>{product.name}</p>
-              <div className="text-center">
-              <img src={product.imageURL} alt ="" className="product-img" />
-              </div>
-            </div>
-            <div className="product-actions">
-              <h2>{product.price} Rs/-</h2>
-              <div className="d-flex">
-                <button className="mx-2" onClick={()=>addtocart(product)}>ADD TO CART</button>
-                <button onClick={()=>navigate(`productinfo/${product.id}`)}>VIEW</button>
+      <div className="container">
+        <div className="d-flex w-50">
+          <input
+            type="text"
+            value={searchKey}
+            onChange={(e) => setSearchKey(e.target.value)}
+            className="form-control"
+            placeholder="search items"
+          />
+          <select
+            className="form-control"
+            value={filterType}
+            onChange={(e) => setFilterType(e.target.value)}
+          >
+            <option value="">All</option>
+            <option value="">eletronics</option>
+            <option value="">fashion</option>
+            <option value="">mobiles</option>
+          </select>
+        </div>
+        <div className="row">
+          {products.filter(obj=>obj.name.toLowerCase().includes(searchKey)).map((product) => {
+            return (
+              <div className="col-md-4" key={product.id}>
+                <div className="m-2 p-1 product position-relative">
+                  <div className="product-content">
+                    <p>{product.name}</p>
+                    <div className="text-center">
+                      <img
+                        src={product.imageURL}
+                        alt=""
+                        className="product-img"
+                      />
+                    </div>
+                  </div>
+                  <div className="product-actions">
+                    <h2>{product.price} Rs/-</h2>
+                    <div className="d-flex">
+                      <button
+                        className="mx-2"
+                        onClick={() => addtocart(product)}
+                      >
+                        ADD TO CART
+                      </button>
+                      <button
+                        onClick={() => navigate(`productinfo/${product.id}`)}
+                      >
+                        VIEW
+                      </button>
+                    </div>
+                  </div>
                 </div>
-            </div>
-            </div>
-          </div>
-        })}
+              </div>
+            );
+          })}
+        </div>
       </div>
-
-    </div>
-
-      
     </Layout>
   );
 }
